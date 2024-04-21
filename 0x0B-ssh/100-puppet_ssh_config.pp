8098a3f849cb0b4d7fb35765bce9c32402c078b6
PaulaMaina <kabura.mainap@gmail.com>
2024-04-21 09:15:37 +0300

    Client configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,14 +3,14 @@ include stdlib
 
 file_line { 'Turn off passwd auth':
   ensure  => present,
-  path    => 'etc'ssh'ssh_config'
+  path    => '/etc/ssh/ssh_config'
   line    => '    PasswordAuthentication no',
   replace => true,
 }
 
 file_line { 'Declare identity file':
   ensure  => present,
-  path    => 'etc/ssh/ssh_config',
+  path    => '/etc/ssh/ssh_config',
   line    => '    IdentifyFile ~/.ssh/school'
   replace => true,
 }
