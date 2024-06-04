75680c19a87c226bbdc017122b1eec6e861c8f46
PaulaMaina <kabura.mainap@gmail.com>
2024-06-04 10:26:52 -0700

    Top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This function prints the titles of the first 10 hot posts"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the total number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                       "AppleWebKit/537.36 (KHTML, like Gecko) "
+                       "Chrome/91.0.4472.124 Safari/537.36")
+    }
+    params = {"limit": 10}
+
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        if response.status_code == 404:
+            print("None")
+            return
+
+        data = response.json()
+        results = data.get("data")
+        for post in results.get("children"):
+            print(post.get("data").get("title"))
+    except requests.RequestException as err:
+        print("An error occurred: ".format(err))
