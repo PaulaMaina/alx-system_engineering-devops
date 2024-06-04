3ae071a9f91d59e2e5a29dc1e84f776d86425a2b
PaulaMaina <kabura.mainap@gmail.com>
2024-06-04 08:08:10 -0700

    Reddit subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     """Retuns the total number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            "User-Agent": "Chrome/51.0.2704.103"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
