eaab02d83d1f28f7108f4a427a679434653cfbe4
PaulaMaina <kabura.mainap@gmail.com>
2024-06-04 08:14:32 -0700

    Reddit subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,12 +8,10 @@ import requests
 def number_of_subscribers(subreddit):
     """Retuns the total number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-            "User-Agent": "Chrome/51.0.2704.103"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    headers = {"User-Agent": "Custom"}
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    else:
         return 0
-    data = response.json().get('data')
-    subscribers = data.get('subscribers')
-    return subscribers
