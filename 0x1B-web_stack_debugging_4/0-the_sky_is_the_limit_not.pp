dffadca6062ddc8615fcd2d2ced2b7fd0a80e0f4
PaulaMaina <kabura.mainap@gmail.com>
2024-06-13 02:42:49 -0700

    Sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # This script increases the amount of traffic an NGINX server can handle
 exec { 'fix--for-nginx' :
-  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service restart',
+  command => 'sed -i "s/15/2000/" /etc/default/nginx && sudo service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
