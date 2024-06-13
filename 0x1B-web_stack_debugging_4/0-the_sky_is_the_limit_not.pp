ce852f7908c2cd2da1f15b981e1c14510a4224d3
PaulaMaina <kabura.mainap@gmail.com>
2024-06-13 02:32:26 -0700

    Web stack debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,5 @@
 # This script increases the amount of traffic an NGINX server can handle
 exec { 'fix-nginx' :
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-exec { 'restart-nginx':
-  command => 'nginx restart',
-  path    => 'etc/init.d/'
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
